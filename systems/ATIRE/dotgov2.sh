8ff52bb4b94224775528b1064ea2c2741fe41fa4
Matt Crane <mcrane@cs.otago.ac.nz>
2015-06-02 10:47:37 +1200

    Added a comment showing search time extraction

--- systems/ATIRE/dotgov2.sh
+++ systems/ATIRE/dotgov2.sh
@@ -16,9 +16,12 @@ for queries in "701-750" "751-800" "801-850"
 do
 	query_file=../../../topics-and-qrels/topics.${queries}.txt
 	qrel_file=../../../topics-and-qrels/qrels.${queries}.txt
+	stat_file=${queries}.search_stats.txt
 	run_file=atire.${queries}.txt
 
 	#./bin/atire -findex quantized.aspt -M -sa -QN:t -q ${query_file} -et -l1000 -oatire.${queries}.speed.txt -iatire -nquantized > ${queries}.speed.search_stats.txt
-	./bin/atire -Qr -sa -QN:t -q ${query_file} -et -l1000 -o${run_file} -iatire > ${queries}.search_stats.txt
+	./bin/atire -Qr -sa -QN:t -q ${query_file} -et -l1000 -o${run_file} -iatire > ${stat_file}
 	../../../eval/trec_eval.9.0/trec_eval ${qrel_file} ${run_file}
+
+	#grep 'Total Time to Search' ${stat_file} | sed \$d
 done
