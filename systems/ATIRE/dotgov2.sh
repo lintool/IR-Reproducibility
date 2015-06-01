eb8e405412a43aa8afb627e7fed0ff136730c413
Matt Crane <mcrane@cs.otago.ac.nz>
2015-06-02 10:36:40 +1200

    Added trec_eval to atire script

--- systems/ATIRE/dotgov2.sh
+++ systems/ATIRE/dotgov2.sh
@@ -9,11 +9,16 @@ cd atire
 
 make
 
-./bin/index -N1000000 -sa -rrtrec -iscrub:an -ts -QBM25 -q -findex quantized.aspt ${GOV2_FILES[@]} | tee quantized.indexing.txt
+#./bin/index -N1000000 -sa -rrtrec -iscrub:an -ts -QBM25 -q -findex quantized.aspt ${GOV2_FILES[@]} | tee quantized.indexing.txt
 ./bin/index -N1000000 -sa -rrtrec -iscrub:an -ts ${GOV2_FILES[@]} | tee indexing.txt
 
 for queries in "701-750" "751-800" "801-850"
 do
-	./bin/atire -findex quantized.aspt -M -sa -QN:t -q ../../../topics-and-qrels/topics.${queries}.txt -et -l1000 -oatire.${queries}.speed.txt -iatire -nquantized > ${queries}.speed.search_stats.txt
-	./bin/atire -Qr -sa -QN:t -q ../../../topics-and-qrels/topics.${queries}.txt -et -l1000 -oatire.${queries}.unquantized.txt -iatire -nunquantized > ${queries}.precision.search_stats.txt
+	query_file=../../../topics-and-qrels/topics.${queries}.txt
+	qrel_file=../../../topics-and-qrels/qrels.${queries}.txt
+	run_file=atire.${queries}.txt
+
+	#./bin/atire -findex quantized.aspt -M -sa -QN:t -q ${query_file} -et -l1000 -oatire.${queries}.speed.txt -iatire -nquantized > ${queries}.speed.search_stats.txt
+	./bin/atire -Qr -sa -QN:t -q ${query_file} -et -l1000 -o${run_file} -iatire > ${queries}.search_stats.txt
+	../../../eval/trec_eval.9.0/trec_eval ${qrel_file} ${run_file}
 done
