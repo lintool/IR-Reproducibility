23924ef5c01e3c8191d9bc0bd60f87a626236e1e
Matt Crane <mcrane@cs.otago.ac.nz>
2015-06-19 17:53:46 +1200

    Added top-k serach, and quantised indexing to atire script

--- systems/ATIRE/dotgov2.sh
+++ systems/ATIRE/dotgov2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ef
+set -f
 
 source ../common.sh
 
@@ -11,19 +11,20 @@ cd atire
 
 make USE_PRINT_TIME_NO_CONVERSION=1
 
-stdbuf -oL ./bin/index -N1000000 -sa -rrtrec -iscrub:an -ts -QBM25 -q -findex quantized.aspt ${GOV2_FILES[@]} | tee quantized.indexing.txt
-stdbuf -oL ./bin/index -N1000000 -sa -rrtrec -iscrub:an -ts ${GOV2_FILES[@]} | tee indexing.txt
+stdbuf -oL ./bin/index -N1000000 -sa -rrtrec -iscrub:an -findex index.aspt ${GOV2_FILES[@]} | tee.indexing.txt
+stdbuf -oL ./bin/index -N1000000 -sa -rrtrec -iscrub:an -QBM25 -q -findex quantized.aspt ${GOV2_FILES[@]} | tee quantized.indexing.txt
 
-for queries in "701-750" "751-800" "801-850"
+for index in "index.aspt" "quantized.aspt"
 do
-	query_file=../$TOPICS_QRELS/topics.${queries}.txt
-	qrel_file=../$TOPICS_QRELS/qrels.${queries}.txt
-	stat_file=${queries}.search_stats.txt
-	run_file=atire.${queries}.txt
+	for queries in "701-750" "751-800" "801-850"
+	do
+		query_file=../$TOPICS_QRELS/topics.${queries}.txt
+		qrel_file=../$TOPICS_QRELS/qrels.${queries}.txt
+		stat_file=${index}.${queries}.search_stats.txt
+		run_file=atire.${index}.${queries}.txt
+		eval_file=eval.${index}.${queries}.txt
 
-	./bin/atire -findex quantized.aspt -sa -QN:t -q ${query_file} -et -l1000 -oquantized.${run_file} -iatire > quantized.${stat_file}
-	../$TREC_EVAL ${qrel_file} quantized.${run_file} >> atire.trec_eval
-
-	./bin/atire -Qr                    -sa -QN:t -q ${query_file} -et -l1000 -o${run_file}           -iatire > ${stat_file}
-	../$TREC_EVAL ${qrel_file} ${run_file}           >> atire.trec_eval
+		./bin/atire -findex ${index} -sa -QN:t -k1000 -q ${query_file} -et -l1000 -o${run_file} -iatire > ${index}.${stat_file}
+		../$TREC_EVAL ${qrel_file} ${run_file} > ${eval_file}
+	done
 done
