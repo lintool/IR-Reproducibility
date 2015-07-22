563d246b428dd7e9934b917d29c0e94436d06eae
Matt Crane <mcrane@cs.otago.ac.nz>
2015-07-22 19:36:26 +1200

    Added additional evaluation tools
    statMAP for 2009 queries
    ERR, nDCG for 2010-2014

--- systems/common.sh
+++ systems/common.sh
@@ -4,6 +4,8 @@ CW12B_LOCATION=/media/ClueWeb12/DiskB
 
 TOPICS_QRELS=../../topics-and-qrels/
 TREC_EVAL=../../eval/trec_eval.9.0/trec_eval
+SAP_EVAL=../../eval/statAP_MQ_eval_v3.pl
+GD_EVAL=../../eval/gdeval
 
 # define JAVA_HOME for those tools that need it
 export JAVA_HOME='/usr/lib/jvm/java-8-oracle/'
@@ -12,4 +14,17 @@ export JAVA_HOME='/usr/lib/jvm/java-8-oracle/'
 if [[ ! -f ${TREC_EVAL} ]]; then
 	tar xzf ../../eval/trec_eval.9.0.tar.gz -C ../../eval
 	make -C ../../eval/trec_eval.9.0/
+fi
+
+# Get statMAP eval tool for the 2009 queries
+if [[ ! -f ${SAP_EVAL} ]]; then
+	curl http://trec.nist.gov/data/web/09/statAP_MQ_eval_v3.pl > ${SAP_EVAL}
+	chmod +x ${SAP_EVAL}
+fi
+
+# Get the gdeval tool for ERR, nDCG
+# This is the latest version I could find
+if [[ ! -f ${GD_EVAL} ]]; then
+	curl https://raw.githubusercontent.com/trec-web/trec-web-2014/master/src/eval/gdeval.pl > ${GD_EVAL}
+	chmod +x ${GD_EVAL}
 fi
