aa3d84de6042279f39b2cda10781df37677b9a94
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2015-08-01 13:11:13 +0000

    Fixes to pure BM25 runs

--- systems/MG4J/cw12-bm25.sh
+++ systems/MG4J/cw12-bm25.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 # Plain BM25 on disjunctive queries (mainly as a baseline)
 
@@ -13,17 +13,17 @@ for queries in "201-250" "251-300"
 do
 	topics=$TOPICS_QRELS/topics.web.$queries.txt
 	qrels=$TOPICS_QRELS/qrels.web.$queries.txt
-	err=err-bm25.$queries.txt
-	run=run-bm25.$queries.txt
+	err=err.$queries.txt
+	run=run.$queries.txt
 
 	# Extract titles, minimal massaging (no stopwords, U.S. => U S, etc.)
-	fgrep -A1 "<query>" $topics | sed 's/<query>//;s/--//;/^[[:space:]]*$/d;s/[[:space:]]*$//;s/^[[:space:]]*//' | sed "s/-/ /g;s/U.S./U S/;s/'s//" | sed 's/\<\(in\|to\|of\|on\|for\|and\|at\)\>//g' > titles.$queries.txt
+	fgrep "<query>" $topics | sed 's/<.\?query>//g;s/--//;/^[[:space:]]*$/d;s/[[:space:]]*$//;s/^[[:space:]]*//' | sed "s/-/ /g;s/U.S./U S/;s/'s//" | sed 's/\<\(a\|the\|in\|to\|of\|on\|for\|and\|at\)\>//g' > titles.$queries.txt
 	# Generate input files
-	cat <(echo -e "\$score BM25Scorer(1.2,0.3)\n\$limit 1000\n\$divert $run\n\$mplex off") <(sed -e 's/[ ]\+/|/g' <titles.$queries.txt | awk "BEGIN {i = ${queries%-*} } { print \"\$mode trec \" i \" mg4jAuto\"; print; i = i + 1; }" ) >in-bm25.$queries.txt
+	cat <(echo -e "\$score BM25Scorer(1.2,0.3)\n\$limit 1000\n\$divert $run\n\$mplex off") <(sed -e 's/[ ]\+/|/g' <titles.$queries.txt | awk "BEGIN {i = ${queries%-*} } { print \"\$mode trec \" i \" mg4jAuto\"; print; i = i + 1; }" ) >in.$queries.txt
 
-	java -server it.unimi.di.big.mg4j.query.Query $WORK_DIR/cw12-text -T $WORK_DIR/cw12.titles <in-bm25.$queries.txt 2>$err
+	java -server it.unimi.di.big.mg4j.query.Query $WORK_DIR/cw12-text -T $WORK_DIR/cw12.titles <in.$queries.txt 2>$err
 
-	./trec_eval $qrels $run >eval-bm25.$queries.txt
+	$TREC_EVAL $qrels $run >eval.$queries.txt
 
-	grep ms\; $err | cut -d' ' -f6 | paste -d+ -s | bc -l >time-bm25.$queries.txt
+	grep ms\; $err | cut -d' ' -f6 | paste -d+ -s | bc -l >time.$queries.txt
 done
