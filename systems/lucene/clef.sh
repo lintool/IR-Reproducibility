ca2e588d74f782b06cd463c7171c0216d4427322
Emanuele Di Buccio <emanuele.dibuccio@gmail.com>
2015-08-12 17:11:31 +0200

    Fixed clef.sh script

--- systems/lucene/clef.sh
+++ systems/lucene/clef.sh
@@ -1,36 +1,36 @@
 source ../common.sh
 
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home
+if [ -z "$CLEF_LOCATION" ]; then 
+	echo "The location of the CLEF Test Collections should be specified";
+else 
 
-CLEF_LOCATION=/Users/dibuccio/Projects/research/activities/collections/CLEF/corpora
+	if [[ ! -f clef/target/lucene-clef-1.0-jar-with-dependencies.jar ]]; then
+		echo "Compiling lucene-clef project..."
+		cd clef
+		mvn clean compile assembly:single
+		cd ..
+	fi
 
-rm -r clef/target
+	cd ../../
 
-if [[ ! -f clef/target/lucene-clef-1.0-jar-with-dependencies.jar ]]; then
-	echo "Compiling lucene-clef project..."
-	cd clef
-	mvn clean compile assembly:single
-	cd ..
-fi
+	ROOT_PATH=$(pwd);
 
-cd ../../
+	mkdir -p $ROOT_PATH/runs
 
-ROOT_PATH=$(pwd);
+	SYSTEM_PATH=$ROOT_PATH/systems/lucene;
 
-mkdir -p $ROOT_PATH/runs
+	cd $SYSTEM_PATH
 
-SYSTEM_PATH=$ROOT_PATH/systems/lucene;
+	while read line;do
+		# load indexing and retrieval options from the clef_runs file
+		lang=$(echo "$line" | cut -d$'\t' -f1);
+		use_stemmer=$(echo "$line" | cut -d$'\t' -f2);
+		use_stoplist=$(echo "$line" | cut -d$'\t' -f3);
+		model=$(echo "$line" | cut -d$'\t' -f4);
+		
+		sh $SYSTEM_PATH/clef_experiments.sh -l $lang -cp $CLEF_LOCATION -stm $use_stemmer -sl $use_stoplist -r $model
 
-cd $SYSTEM_PATH
+	done < $SYSTEM_PATH/clef_runs
 
-while read line;do
-	# load indexing and retrieval options from the clef_runs file
-	lang=$(echo "$line" | cut -d$'\t' -f1);
-	use_stemmer=$(echo "$line" | cut -d$'\t' -f2);
-	use_stoplist=$(echo "$line" | cut -d$'\t' -f3);
-	model=$(echo "$line" | cut -d$'\t' -f4);
-	
-	sh $SYSTEM_PATH/clef_experiments.sh -l $lang -cp $CLEF_LOCATION -stm $use_stemmer -sl $use_stoplist -r $model
-
-done < $SYSTEM_PATH/clef_runs
+fi	
 
