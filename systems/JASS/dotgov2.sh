2bca729cce558e17a16b14314202f8f40ef1db9a
Matt Crane <mcrane@cs.otago.ac.nz>
2015-09-08 02:21:41 +0000

    Updated gov2 script to overwrite previous results

--- systems/JASS/dotgov2.sh
+++ systems/JASS/dotgov2.sh
@@ -17,21 +17,21 @@ echo "'Indexing' took:" $((END - START)) "seconds"
 
 for queries in "701-750" "751-800" "801-850"
 do
-	query_file=../$TOPICS_QRELS/topics.web.${queries}.txt
-	qrel_file=../$TOPICS_QRELS/qrels.web.${queries}.txt
+	query_file=../$TOPICS_QRELS/topics.${queries}.txt
+	qrel_file=../$TOPICS_QRELS/qrels.${queries}.txt
 	stat_file=${index}.${queries}.search_stats.txt
 	run_file=${queries}.txt
 	eval_file=eval.${queries}.txt
 
-	./trec2query/trec2query ${query_file} q > ${queries}.txt
+	./trec2query/trec2query ${query_file} t -s s >| ${queries}.txt
 
 	echo "Searching queries ${queries} to 1B postings"
-	./jass ${queries}.txt 1000 1000000000 -d > comp.${stat_file}
+	./jass ${queries}.txt 1000 1000000000 -d >| comp.${stat_file}
 	mv ranking.txt comp.jass.${run_file}
-	../$TREC_EVAL ${qrel_file} comp.jass.${run_file} > comp.${eval_file}
+	../$TREC_EVAL ${qrel_file} comp.jass.${run_file} >| comp.${eval_file}
 
 	echo "Searching queries ${queries} to 2.5M postings"
-	./jass ${queries}.txt 1000 2500000 -d > heur.${stat_file}
+	./jass ${queries}.txt 1000 2500000 -d >| heur.${stat_file}
 	mv ranking.txt heur.jass.${run_file}
-	../$TREC_EVAL ${qrel_file} heur.jass.${run_file} > heur.${eval_file}
+	../$TREC_EVAL ${qrel_file} heur.jass.${run_file} >| heur.${eval_file}
 done
